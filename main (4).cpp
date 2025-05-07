e380c92247db5e5c1f29ed74c8f3117f632998c2
pumputis777 <facundopumputis@gmail.com>
2025-05-07 20:04:21 -0300

    main (4).cpp

--- main (4).cpp
+++ main (4).cpp
@@ -11,31 +11,29 @@ using namespace std;
 int main()
 {
    // Declarar las variables
-    float num1;
-    float num2;
-    float num3;
-    float num4;
-    float suma;
-    float promedio;
+    float num1; // Declaro la primera variable
+    float num2; // Declaro la segunda variable
+    float num3; // Declaro la tercera variable
+    float num4; // Declaro la cuarta variable
+    float suma; // Declaro la quinta variable
+    float promedio; // Declaro la sexta variable
     
-    cout << "este programa suma 4 numeros y despues hace el proemdio" <<endl; 
-    // Solicitar al usuario que ingrese los cuatro números
+    cout << "este programa suma 4 numeros y despues hace el proemdio" <<endl; // le indica al usuario lo que hace el programa 
+    // Solicita al usuario que ingrese los cuatro números
     cout << "Ingrese el primer número: ";
-    cin >> num1;
+    cin >> num1; // guarda el valor dado por el usuario en la variable num1
     cout << "Ingrese el segundo número: ";
-    cin >> num2;
+    cin >> num2; // guarda el valor dado por el usuario en la variable num2
     cout << "Ingrese el tercer número: ";
-    cin >> num3;
+    cin >> num3; // guarda el valor dado por el usuario en la variable num3
     cout << "Ingrese el cuarto número: ";
-    cin >> num4;
+    cin >> num4; // guarda el valor dado por el usuario en la variable num4
 
-    // Calcular la suma de los cuatro números
-    suma = num1 + num2 + num3 + num4;
+    suma = num1 + num2 + num3 + num4; // Calcula la suma de las cuatro variables y guarda el resultado en la variable suma
 
-    // Calcular el promedio
-    promedio = suma / 4;
+    promedio = suma / 4; // Calcula el promedio y guarda el resultado en la variable promedio
 
-    // Mostrar los resultados
+    // Muestra los resultados de la suma y del promedio
     cout << "La suma de los cuatro números es: " << suma << endl;
     cout << "El promedio de los cuatro números es: " << promedio << endl;
 
